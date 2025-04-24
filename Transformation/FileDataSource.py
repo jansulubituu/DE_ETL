b2e059bb034ab645275e0c76724484e6455958f5
jansulubituu <nvthanh.ytbg@gmail.com>
2025-04-25 03:52:03 +0700

    btvn_23_4

--- Transformation/FileDataSource.py
+++ Transformation/FileDataSource.py
@@ -8,12 +8,28 @@ conf = SparkConf().setAppName("DE").setMaster("local[*]").set("spark.executor.me
 
 sc = SparkContext(conf=conf)
 
-fileRdd = sc.textFile(r"C:\Users\ADMIN\PycharmProjects\PythonProject3\RDD\Transformation\data\data.txt")
+fileRdd = sc.textFile(r"C:\Users\ADMIN\PycharmProjects\PythonProject3\RDD\Transformation\data\data.txt") \
+    .map(lambda x : x.lower()) \
+    .flatMap(lambda x: x.split(" ")) \
+    .map(lambda x: (x, 1) )\
+    .reduceByKey(lambda x, y: x + y) \
+    # .map(lambda x: (x[1], x[0])) \
+    # .sortByKey(ascending=False)
 
-print(fileRdd.collect()) #tra ve 1 list
-print(fileRdd.getNumPartitions()) # tra ve so pa
-print(fileRdd.count())
-print(fileRdd.glom().collect())
+rdd2 = sc.parallelize([("the",1)])
 
-for part in fileRdd.glom().collect():
-    print(part)
+rdd = fileRdd.join(rdd2)
+print(rdd.collect())
+
+# print(fileRdd.collect()) #tra ve 1 list
+# print(fileRdd.getNumPartitions()) # tra ve so pa
+# print(fileRdd.count())
+# print(fileRdd.glom().collect())
+#
+# for part in fileRdd.glom().collect():
+#     print(part)
+
+#cho day  so nguyen duong lon xon  => tim ra so nguyen duong nao xuat hien bao nhieu lan
+
+# ex: 0,0 5,3,4,4,5,3,432,42,4,24,234,234,23,4,234,654,64565,6,64,64,57,5,438568,6,5,2,683456,
+
